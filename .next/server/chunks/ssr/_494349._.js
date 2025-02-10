f6ef54c8fb5721df3484ca96bea405e0be223621
M-Asim-69 <asimali6506@gmail.com>
2025-02-10 10:25:50 -0800

    cxcvx

--- .next/server/chunks/ssr/_494349._.js
+++ .next/server/chunks/ssr/_494349._.js
@@ -57,7 +57,7 @@ async function Page() {
                     columnNumber: 3
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "grid grid-cols-3 gap-6 p-10",
+                    className: "grid md:grid-cols-3 sm:gap-8 gap-6 p-10",
                     children: Videos.items.map((video)=>/*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
                             src: `https://www.youtube.com/embed/${video.id.videoId}`,
                             allowFullScreen: true,
