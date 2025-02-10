6edee76b7af80fc05c3d3c563665584bf85de985
M-Asim-69 <asimali6506@gmail.com>
2025-02-10 10:22:50 -0800

    Fixed

--- src/app/Videos/page.jsx
+++ src/app/Videos/page.jsx
@@ -8,7 +8,7 @@ async function Page() {
     <>
 <div className='bg-gradient-to-b from-green-950 via-green-600 to-green-950 pt-36 pb-28'>
   <h1 className='text-6xl font-medium text-white text-center'>Videos</h1>
-<div className="grid sm:grid-cols-1 grid-col-3  sm:gap-8 gap-6 p-10">
+<div className="grid sm:grid-cols-1 md:grid-col-3 lg:grid-col-3 sm:gap-8 gap-6 p-10">
         {Videos.items.map((video) => (
           <iframe
             key={video.id.videoId} // Adding a key for React optimization
